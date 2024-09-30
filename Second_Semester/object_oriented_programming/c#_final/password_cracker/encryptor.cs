0d867bc1d9130ad0da36afa07ae85286d90e19a0
nayemhasan45 <nayemhasan75907@gmail.com>
2024-09-30 20:37:20 +0100

    third semester first assignment of database

---
+++ Second_Semester/object_oriented_programming/c#_final/password_cracker/encryptor.cs
@@ -0,0 +1,16 @@
+﻿namespace password_cracker.Models
+{
+    public static class passwordEncryptor
+    {
+        private static readonly string Salt = "STATIC_SALT";
+        public static string Encrypt(string pass)
+        {
+            using(var sha256=System.Security.Cryptography.SHA256.Create())
+            {
+                var saltPass = pass + Salt;
+                byte[]bytes=sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(saltPass));
+                return System.Convert.ToBase64String(bytes);
+            }
+        }
+    }
+}
