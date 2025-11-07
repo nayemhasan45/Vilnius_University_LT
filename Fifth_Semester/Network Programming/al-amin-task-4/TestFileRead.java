a5246f9ec6d59b88c71a974d3074fb449eecf99d
nayemhasan45 <nayemhasan75907@gmail.com>
2025-11-07 11:56:57 +0200

    assignmet 6 done

--- Fifth_Semester/Network Programming/al-amin-task-4/TestFileRead.java
+++
@@ -1,8 +0,0 @@
-public class TestFileRead {
-    public static void main(String[] args) {
-        ReadFile app = new ReadFile();
-        app.openFile();
-        app.readPrisms();
-        app.closeFile();
-    }
-}
--- Fifth_Semester/Network Programming/al-amin-task-4/TestFileWrite.java
+++
@@ -1,8 +0,0 @@
-public class TestFileWrite {
-    public static void main(String[] args) {
-        CreateFile app = new CreateFile();
-        app.openFile();
-        app.add7Prisms();
-        app.closeFile();
-    }
-}
